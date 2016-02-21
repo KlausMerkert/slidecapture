dde83db1f9c0c150cfe02e9ea995cdb9e7f049a6
KlausMerkert <klaus@merkert.info>
2016-02-21 17:36:30 +0100

    some renamings

--- dia0.py
+++ dia0.py
@@ -4,24 +4,26 @@ from PySide.QtGui import *
  
 app = QApplication(sys.argv)
 
-w = QWidget()
-w.setWindowTitle('Dias einlesen')
-w.resize(1200,800)
+mainwindow = QWidget()
+mainwindow.setWindowTitle('Dias einlesen')
+mainwindow.resize(1200,800)
 
-l = QLabel(w)
-l.move(5,5)
-l.resize(1024,768)
+PicLabel = QLabel(mainwindow)
+PicLabel.move(5,5)
+PicLabel.resize(1024, 768)
 p = QPixmap('aktuell.jpg')
 p = p.scaledToHeight(768)
-l.setPixmap(p)
+PicLabel.setPixmap(p)
 
-x  = 1100
-y  = 20
-dy = 30
+x  = 1100 # linke Kante der Eingabeelemente
+y  = 20   # Abstand der Eingabeelemente von oben
+dy = 30   # vertikaler Abstand der Eingabeelemente
 
-def lWidth(lName):
-    return lName.fontMetrics().boundingRect(lName.text()).width()
-    
+
+def getLabelWidth(Label):
+    return Label.fontMetrics().boundingRect(Label.text()).width()
+
+   
 def showCreateDate():
     os.system('exiftool -CreateDate aktuell.jpg > CreateDate.txt')
     datei = open('CreateDate.txt','r')
@@ -29,6 +31,7 @@ def showCreateDate():
     s = s.replace('Create Date                     :','Create Date:')
     datei.close()
     lCreateDate.setText(s)
+
     
 def showKeywords():
     os.system('exiftool -mwg:keywords aktuell.jpg > keywords.txt')
@@ -38,12 +41,13 @@ def showKeywords():
     datei.close()
     lKeywords.setText(s)
 
+
 def on_Dia():
     print('mache ein Foto!')
     showCreateDate()
     showKeywords()
     	
-bDia = QPushButton(w)
+bDia = QPushButton(mainwindow)
 bDia.setText('mache Foto')
 bDia.move(x,y)
 bDia.clicked.connect(on_Dia)
@@ -51,7 +55,7 @@ bDia.clicked.connect(on_Dia)
 def on_Invert():
     print('invertiere das Foto!')
     	
-bInvert = QPushButton(w)
+bInvert = QPushButton(mainwindow)
 bInvert.setText('invertiere')
 bInvert.move(x,y+1*dy)
 bInvert.clicked.connect(on_Invert)
@@ -60,10 +64,10 @@ def on_Mirror():
     os.system('convert aktuell.jpg -flop aktuell.jpg')
     p = QPixmap('aktuell.jpg')
     p = p.scaledToHeight(768)
-    l.setPixmap(p)
+    PicLabel.setPixmap(p)
     print('spiegele das Foto!')
     	
-bMirror = QPushButton(w)
+bMirror = QPushButton(mainwindow)
 bMirror.setText('spiegele')
 bMirror.move(x,y+2*dy)
 bMirror.clicked.connect(on_Mirror)
@@ -71,81 +75,81 @@ bMirror.clicked.connect(on_Mirror)
 def on_Turn():
     print('drehe das Foto!')
     	
-bTurn = QPushButton(w)
+bTurn = QPushButton(mainwindow)
 bTurn.setText('drehe')
 bTurn.move(x,y+3*dy)
 bTurn.clicked.connect(on_Turn)
 
-eJahr = QLineEdit(w)
+eJahr = QLineEdit(mainwindow)
 eJahr.move(x,y+4*dy)
 eJahr.setInputMask('0000')
 eJahr.setText('1983')
 eJahr.setFixedWidth(50)
-lJahr = QLabel(w)
+lJahr = QLabel(mainwindow)
 lJahr.setText('Jahr')
-lJahr.move(x-lWidth(lJahr),y+4*dy+5)
+lJahr.move(x-getLabelWidth(lJahr),y+4*dy+5)
 
-eMonat = QLineEdit(w)
+eMonat = QLineEdit(mainwindow)
 eMonat.move(x,y+5*dy)
 eMonat.setInputMask('00')
 eMonat.setText('05')
 eMonat.setFixedWidth(50)
-lMonat = QLabel(w)
+lMonat = QLabel(mainwindow)
 lMonat.setText('Monat')
-lMonat.move(x-lWidth(lMonat),y+5*dy+5)
+lMonat.move(x-getLabelWidth(lMonat),y+5*dy+5)
 
-eTag = QLineEdit(w)
+eTag = QLineEdit(mainwindow)
 eTag.move(x,y+6*dy)
 eTag.setInputMask('00')
 eTag.setText('17')
 eTag.setFixedWidth(50)
-lTag = QLabel(w)
+lTag = QLabel(mainwindow)
 lTag.setText('Tag')
-lTag.move(x-lWidth(lTag),y+6*dy+5)
+lTag.move(x-getLabelWidth(lTag),y+6*dy+5)
 
 def on_TimeTag():
     print('setze TimeTag!')
     	
-bTimeTag = QPushButton(w)
+bTimeTag = QPushButton(mainwindow)
 bTimeTag.setText('setze Zeit')
 bTimeTag.move(x,y+7*dy)
 bTimeTag.clicked.connect(on_TimeTag)
 
-eKeywords = QLineEdit(w)
+eKeywords = QLineEdit(mainwindow)
 eKeywords.move(x,y+8*dy)
 eKeywords.setText('Hausbau')
 eKeywords.setFixedWidth(90)
-lKeywords = QLabel(w)
+lKeywords = QLabel(mainwindow)
 lKeywords.setText('Keywords')
-lKeywords.move(x-lWidth(lKeywords),y+8*dy+5)
+lKeywords.move(x-getLabelWidth(lKeywords),y+8*dy+5)
 
 def on_Keywords():
     print('setze Keywords!')
     	
-bKeywords = QPushButton(w)
+bKeywords = QPushButton(mainwindow)
 bKeywords.setText('setze Tags')
 bKeywords.move(x,y+9*dy)
 bKeywords.clicked.connect(on_Keywords)
 
-eName = QLineEdit(w)
+eName = QLineEdit(mainwindow)
 eName.move(x,y+10*dy)
 eName.setText('dia00000')
 eName.setFixedWidth(90)
-lName = QLabel(w)
+lName = QLabel(mainwindow)
 lName.setText('Name')
-lName.move(x-lWidth(lName),y+10*dy+5)
+lName.move(x-getLabelWidth(lName),y+10*dy+5)
 
-lCreateDate = QLabel(w)
+lCreateDate = QLabel(mainwindow)
 lCreateDate.setFixedWidth(300)
 lCreateDate.setText('CreateDate: ?')
 lCreateDate.move(10,778)
 
-lKeywords = QLabel(w)
+lKeywords = QLabel(mainwindow)
 lKeywords.setFixedWidth(400)
 lKeywords.setText('Keywords: ?')
 lKeywords.move(320,778)
 
-w.show()
+mainwindow.show()
 
 app.exec_()
 sys.exit()
